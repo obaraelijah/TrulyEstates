a5af206bd2219298a219d29f5f390b703b11d4d0
elijah obara <elijahobara357@gmail.com>
2023-04-09 18:56:54 +0300

    Choices

---
+++ listings/choices.py
@@ -0,0 +1,48 @@
+bedroom_choices = {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10":10
+}
+
+price_choices = {
+    "1000": "1000",
+    "2000": "2000",
+    "3000": "3000",
+    "4000": "4000",
+    "5000": "5000",
+    "6000": "6000",
+    "7000": "70000",
+    "8000": "8000",
+    "9000": "9000",
+    "10000": "10k+",
+}
+
+location_choices = {
+    "NAIROBI": "Nairobi",
+    "MOMBASA": "Mombasa",
+    "Kisii": "Kisii",
+    "KAKAMEGA": "Kakamega",
+    "KISUMU": "Kisumu",
+    "MACHALOES": "Machakoes",
+    "NAKURU": "Nakuru",
+    "KERICHO": "Kericho",
+    "ELDORET": "Eldoret",
+    "HOMABAY": "Homabay",
+    "KERUGOYA": "Kerugoya",
+    "MALINDI": "Malindi",
+    "LAMU": "Lamu",
+    "RONGAI": "Rongai",
+    "NAROK": "Narok",
+    "MAASAIMARA": "Maasaimara",
+    "LANGATA": "Langata",
+    "THIKA": "Thika",
+    "EMBU": "Embu",
+    "MURANGA": "Muranga"
+}
