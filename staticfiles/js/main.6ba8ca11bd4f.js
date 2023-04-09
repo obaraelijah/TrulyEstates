f1e0fa42047492af464376c69a389e104ce8df97
elijah obara <elijahobara357@gmail.com>
2023-04-09 10:18:40 +0300

    Hot fix

--- staticfiles/js/main.6ba8ca11bd4f.js
+++
@@ -1,9 +0,0 @@
-const date = new Date();
-document.querySelector(".year").innerHTML = date.getFullYear();
-setTimeout(() => {
-  $("#messages").fadeOut("slow");
-}, 3000);
-
-setTimeout(function() {
-  $(".messages").fadeOut("slow");
-}, 3000);
