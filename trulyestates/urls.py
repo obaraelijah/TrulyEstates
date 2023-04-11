d6f861a9004d3c1610529cab410958f4c70d48e8
elijah obara <elijahobara357@gmail.com>
2023-04-11 08:21:01 +0300

    Urls paths

--- trulyestates/urls.py
+++ trulyestates/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", include("pages.urls")),
     path("account/", include("account.urls")),
-    path("listings/", include("listings.urls"))    
+    path("listings/", include("listings.urls")),
+    path("contacts/", include("contacts.urls")),
+    path('admin/', admin.site.urls),    
 ]
